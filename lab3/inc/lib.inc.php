b81f538a58a70403cfd853a6513472f8298ad385
VoloshinVadim <vadim.ogu@mail.ru>
2025-11-21 08:09:22 +0300

    Create lib.inc.php

---
+++ lab3/inc/lib.inc.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Отрисовывает меню.
+ *
+ * @param array $menu Массив с данными меню
+ * @param bool $vertical Вертикальное (true) или горизонтальное (false)
+ */
+function getMenu(array $menu, bool $vertical = true): void
+{
+    $style = $vertical ? '' : 'display: inline; margin-right: 15px;';
+    echo '<ul>';
+    foreach ($menu as $item) {
+        echo "<li style='{$style}'><a href='{$item['href']}'>{$item['link']}</a></li>";
+    }
+    echo '</ul>';
+}
+
+/**
+ * Отрисовывает таблицу умножения.
+ *
+ * @param int $cols Количество столбцов
+ * @param int $rows Количество строк
+ * @param string $color Цвет заголовков
+ */
+function getTable(int $cols = 10, int $rows = 10, string $color = 'yellow'): void
+{
+    echo "<table border='1' width='200'>";
+    for ($r = 1; $r <= $rows; $r++) {
+        echo "<tr>";
+        for ($c = 1; $c <= $cols; $c++) {
+            $val = $r * $c;
+            if ($r === 1 || $c === 1) {
+                echo "<th style='background-color:{$color}'>{$val}</th>";
+            } else {
+                echo "<td>{$val}</td>";
+            }
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+}
