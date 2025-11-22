00dacb15e796c8891da774a71d4b6571bb979a17
VoloshinVadim <vadim.ogu@mail.ru>
2025-11-22 17:01:39 +0300

    Update bottom.inc.php

--- lab4/site/inc/bottom.inc.php
+++ lab4/site/inc/bottom.inc.php
@@ -1,3 +1,9 @@
 <footer>
-    &copy; Супер Мега Веб-мастер, 2000 &ndash; <?= date('Y') ?>
+    <!-- Нижняя часть страницы -->
+    <?php 
+        $dateArray = getdate(); // Используем getdate, как просили
+        $year = $dateArray['year'];
+    ?>
+    &copy; Супер Мега Веб-мастер, 2000 &ndash; <?= $year ?>
+    <!-- Нижняя часть страницы -->
 </footer>
