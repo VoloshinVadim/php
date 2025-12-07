f43b764f6821bee2b1241f9676944282229ad7dd
VoloshinVadim <vadim.ogu@mail.ru>
2025-12-07 18:13:44 +0300

    Update lib.inc.php

--- lab3/inc/lib.inc.php
+++ lab3/inc/lib.inc.php
@@ -2,10 +2,36 @@
 declare(strict_types=1);
 
 /**
+ * @param int $cols Количество столбцов
+ * @param int $rows Количество строк
+ * @param string $color Цвет заголовков
+ */
+function getTable(int $cols = 10, int $rows = 10, string $color = 'yellow'): void
+{
+    // ИСПОЛЬЗУЕМ CSS вместо border='1' width='200'
+    echo "<table style='border-collapse: collapse; border: 1px solid black; width: 200px;'>";
+    
+    for ($r = 1; $r <= $rows; $r++) {
+        echo "<tr>";
+        for ($c = 1; $c <= $cols; $c++) {
+            $val = $r * $c;
+            
+            // Стили для ячеек тоже задаем через CSS
+            $style = "padding: 5px; border: 1px solid black;";
+            
+            if ($r === 1 || $c === 1) {
+                echo "<th style='{$style} background-color:{$color}'>{$val}</th>";
+            } else {
+                echo "<td style='{$style}'>{$val}</td>";
+            }
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+
+/**
  * Отрисовывает меню.
- *
- * @param array $menu Массив с данными меню
- * @param bool $vertical Вертикальное (true) или горизонтальное (false)
  */
 function getMenu(array $menu, bool $vertical = true): void
 {
@@ -15,29 +41,4 @@ function getMenu(array $menu, bool $vertical = true): void
         echo "<li style='{$style}'><a href='{$item['href']}'>{$item['link']}</a></li>";
     }
     echo '</ul>';
-}
-
-/**
- * Отрисовывает таблицу умножения.
- *
- * @param int $cols Количество столбцов
- * @param int $rows Количество строк
- * @param string $color Цвет заголовков
- */
-function getTable(int $cols = 10, int $rows = 10, string $color = 'yellow'): void
-{
-    echo "<table border='1' width='200'>";
-    for ($r = 1; $r <= $rows; $r++) {
-        echo "<tr>";
-        for ($c = 1; $c <= $cols; $c++) {
-            $val = $r * $c;
-            if ($r === 1 || $c === 1) {
-                echo "<th style='background-color:{$color}'>{$val}</th>";
-            } else {
-                echo "<td>{$val}</td>";
-            }
-        }
-        echo "</tr>";
-    }
-    echo "</table>";
 }
