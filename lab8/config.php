eb8d263463b146bbde59e613903dd7d4b9583c5a
VoloshinVadim <vadim.ogu@mail.ru>
2025-12-07 19:26:25 +0300

    Create config.php

---
+++ lab8/config.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Файл конфигурации базы данных.
+ */
+
+define('DB_HOST', 'localhost');
+// Имя базы данных
+define('DB_NAME', 'f1184189_login_db');
+// Имя пользователя
+define('DB_USER', 'f1184189_login_db');
+define('DB_PASSWORD', 'evewpuwere');
+define('DB_CHARSET', 'utf8mb4');
