9df35b5f3058326db87920f29372fe7fed0a2dd9
VoloshinVadim <vadim.ogu@mail.ru>
2025-12-07 18:17:29 +0300

    Update lib.inc.php

--- lab4/site/inc/lib.inc.php
+++ lab4/site/inc/lib.inc.php
@@ -2,20 +2,27 @@
 declare(strict_types=1);
 
 /**
- * Отрисовывает таблицу умножения.
- * Была getTable, стала drawTable по требованию Лабы 4.
+ * @param int $cols Количество столбцов
+ * @param int $rows Количество строк
+ * @param string $color Цвет заголовков
  */
-function drawTable(int $cols = 10, int $rows = 10, string $color = 'yellow'): void
+function getTable(int $cols = 10, int $rows = 10, string $color = 'yellow'): void
 {
-    echo "<table border='1' width='200'>";
+    // ИСПОЛЬЗУЕМ CSS вместо border='1' width='200'
+    echo "<table style='border-collapse: collapse; border: 1px solid black; width: 200px;'>";
+    
     for ($r = 1; $r <= $rows; $r++) {
         echo "<tr>";
         for ($c = 1; $c <= $cols; $c++) {
             $val = $r * $c;
+            
+            // Стили для ячеек тоже задаем через CSS
+            $style = "padding: 5px; border: 1px solid black;";
+            
             if ($r === 1 || $c === 1) {
-                echo "<th style='background-color:{$color}'>{$val}</th>";
+                echo "<th style='{$style} background-color:{$color}'>{$val}</th>";
             } else {
-                echo "<td>{$val}</td>";
+                echo "<td style='{$style}'>{$val}</td>";
             }
         }
         echo "</tr>";
@@ -23,6 +30,9 @@ function drawTable(int $cols = 10, int $rows = 10, string $color = 'yellow'): vo
     echo "</table>";
 }
 
+/**
+ * Отрисовывает меню.
+ */
 function getMenu(array $menu, bool $vertical = true): void
 {
     $style = $vertical ? '' : 'display: inline; margin-right: 15px;';
